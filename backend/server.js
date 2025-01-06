df3249fb60caa2aa0d9ed6dfde9739a23057c2d6
Pedro M. Dominguez <84252067+xtcedro@users.noreply.github.com>
2025-01-05 21:07:34 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,6 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const appointmentsRoutes = require('./routes/appointments');
-app.use('/api/appointments', appointmentsRoutes);
 
 // Middleware
 app.use(bodyParser.json());

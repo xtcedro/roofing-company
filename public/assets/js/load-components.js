e67d4319db79c102aceb73934cba860b630fd445
Pedro M. Dominguez <84252067+xtcedro@users.noreply.github.com>
2025-01-28 08:04:25 -0800

    Update load-components.js

--- public/assets/js/load-components.js
+++ public/assets/js/load-components.js
@@ -1,24 +1,19 @@
 // Load Header
-fetch('../components/header.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('header').innerHTML = data;
-    })
-    .catch(error => console.error('Error loading header:', error));
+export function loadHeader() {
+    fetch('../../components/header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading header:', error));
+}
 
 // Load Footer
-fetch('../components/footer.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('footer').innerHTML = data;
-    })
-    .catch(error => console.error('Error loading footer:', error));
-
-
- // Load Footer for Spanish pages
-fetch('../components/footer_es.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('footer_es').innerHTML = data;
-    })
-    .catch(error => console.error('Error loading footer:', error));
+export function loadFooter() {
+    fetch('../../components/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading footer:', error));
+}
